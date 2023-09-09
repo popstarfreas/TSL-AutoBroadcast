615790f7a70abfcf003f3b95e87203af6aeb94c2
popstarfreas <6698249+popstarfreas@users.noreply.github.com>
2023-09-09 10:59:41 +0100

    build: Update build to use yarn

--- build.plugin.js
+++ build.plugin.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var child_process = require("child_process");
 
-child_process.execSync("npm i ../../pluginreference", { stdio: "inherit" })
+child_process.execSync("yarn add ../../pluginreference", { stdio: "inherit" })
+child_process.execSync("yarn", { stdio: "inherit" })
 child_process.execSync("npm run build", { stdio: "inherit" })
 fs.renameSync("./build", "./plugin")
