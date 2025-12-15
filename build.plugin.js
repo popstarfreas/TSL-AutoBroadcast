e8d556b8cab562944999359b71098a88ac15e2a0
popstarfreas <popstarfreas2@gmail.com>
2025-12-15 18:54:49 +0000

    fix: Fix installation

--- build.plugin.js
+++ build.plugin.js
@@ -1,6 +1,7 @@
 import fs from "node:fs";
 import child_process from "node:child_process";
 
+child_process.execSync("pnpm i", { stdio: "inherit" })
 child_process.execSync("pnpm remove terrariaserver-lite", { stdio: "inherit" })
 child_process.execSync("pnpm add ../../", { stdio: "inherit" })
 child_process.execSync("pnpm run build", { stdio: "inherit" })
