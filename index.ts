ac1e665e9d83ccf67c787a7c6c1d4387ae8b57ae
popstarfreas <popstarfreas2@gmail.com>
2025-12-17 10:40:10 +0000

    fix: Fix __dirname

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import * as fs from "fs";
 import TerrariaServer from "terrariaserver-lite/terrariaserver";
 import Extension from "terrariaserver-lite/extensions/extension";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 interface Broadcast {
     lines: string[];
